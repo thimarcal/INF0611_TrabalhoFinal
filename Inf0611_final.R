85a308988a48bd726820b69fdb2e65afd67188ed
thiagom <thiagom@motorola.com>
2018-05-01 22:22:33 -0300

    changes precision recall calculation

--- Inf0611_final.R
+++ Inf0611_final.R
@@ -1,7 +1,7 @@
 #################################################
 ## INF-0611 - Trabalho Final
 ##        Rafael Fernando Ribeiro
-##        Thiago Gomes Marçal Pereira
+##        Thiago Gomes Mar?al Pereira
 #################################################
 
 library(ggplot2)
@@ -9,53 +9,12 @@ require(reshape2)
 library(wvtool)
 library(OpenImageR)
 
+#setwd("C:\\Users\\rafae\\Documents\\INF0611_Final")
 setwd("/Users/thiagom/Documents/Studies/Unicamp/MDC/INF-611/Tarefas/INF0611_TrabalhoFinal")
 
 #-----------------------------------------------------------
-# Funções
+# Fun??es
 # ----------------------------------------------------------
-sax.vocab <- list("3"=c(-0.43,0.43), "4"=c(-0.67,0,0.67), "5"=c(-0.84, -0.25, 0.25, 0.84), "6"=c(-0.97,-0.43,0,0.43,0.97), "7"=c(-1.07,-0.57, -0.18, 0.18, 0.57, 1.07))
-
-sax.alpha <- function(values, vocab_size) {
-  res <- c()
-  for (v in values) {
-    count <- 1
-    for (i in sax.vocab[[vocab_size]]) {
-      if (v <= i) {
-        res <- c(res, count)
-        break
-      }
-      count <- count + 1
-    }
-    if (count == length(sax.vocab[[vocab_size]])+1) {
-      res <- c(res, count)
-    }
-  }
-  res
-}
-
-sax.paa <- function (values, size) {
-  
-  res <- c()
-  for (i in c(seq(from =1, to= length(values), by = size))) {
-    res <- c(res, mean(values[i:(i+size-1)]))
-  }
-  res
-}
-
-sax.dist.min <- function (v1, v2, vocab_size, n, w) {
-  
-  res <- c()
-  for (i in c(1:length(v1))) {
-    if (abs(v1[i]-v2[i]) > 1) {
-      res <- c(res, (sax.vocab[[vocab_size]][(max(v1[i],v2[i])-1)] - sax.vocab[[vocab_size]][(min(v1[i],v2[i]))]))
-    }
-    else {
-      res <- c(res,0)
-    }
-  }
-  sqrt(n/w) * sqrt(sum(res^2))
-}
 
 # Recurrence Plot
 recurrence_plot <- function(vector) {
@@ -80,7 +39,7 @@ extrairLBP <- function(img){
 }
 
 #L1 ou Manhattan
-DistL1 <- function(x, y){
+dist.L1 <- function(x, y){
   d = 0
   for (i in c(1:length(x))) {
     d = d+abs(x[i]-y[i])
@@ -88,7 +47,7 @@ DistL1 <- function(x, y){
   return(d)
 }
 
-# Buscar K imagens mais próximas
+# Buscar K imagens mais pr?ximas
 #parametros:
 #  M: matriz que comtem os vetore de carateristicas
 #  query: o nome do arquivo de consulta
@@ -96,23 +55,15 @@ DistL1 <- function(x, y){
 #retorno:
 #  a lista dos nomes das K-imagens mais proximas
 
-buscarMaisProximosL1 <- function(M, query, K){
-  distancias <- apply(M, 2, DistL1, query)
+buscarMaisProximos <- function(M, query, dist.func, K){
+  distancias <- apply(M, 2, dist.func, query)
   
   distancias <- order(distancias, decreasing = FALSE)
   
   return(distancias[1:K])
 }
 
-buscarMaisProximosL2 <- function(M, query, K){
-  distancias <- apply(M, 2, dist.L2, query)
-  
-  distancias <- order(distancias, decreasing = FALSE)
-  
-  return(distancias[1:K])
-}
-
-# Retorna a lista de imagens, baseado nos índices
+# Retorna a lista de imagens, baseado nos ?ndices
 lista_imagens <- function (vector) {
   return (db_original[vector, 1])
 }
@@ -139,40 +90,37 @@ dist.cos <- function(x, y){
 
 # Calculo das distancias
 dist.calc <- function(db, query, dist.func) {
-  
+  print("Calculando distancias")
   qrow <- nrow(query)
   qcol <- ncol(query)
   drow <- nrow(db)
   dcol <- ncol(db)
   
-  l <- list()
-  for (q in 1:qrow) {
-    dists <- c()
-    for (d in 1:drow) {
-      dists <- c(dists, dist.func(query[q,2:qcol], db[d,2:dcol]))
-    }
-    data <- data.frame(db[1:drow,1], dists)
-    colnames(data) <- c("target", "dist")
-    l[[q]] <- data[order(data[,2]),][1:(min(100,drow)),]
+  #l <- list()
+  proximos <- matrix(nrow = length(lbp_queries[1,]), ncol = MAIS_PROXIMOS)
+  for (i in c(1:length(lbp_queries[1,]))) {
+    proximos[i,] <- buscarMaisProximos(lbp_originais, lbp_queries[,i], dist.func, MAIS_PROXIMOS)
+    proximos[i,] <- lista_imagens(proximos[i,])
   }
-  return(l)
+  
+  return(proximos)
 }
 
-calc.precision <- function(q, db, distances, k) {
-  tp <- sum(distances$target[1:k] == q)
+calc.precision <- function(q, distances, k) {
+  tp <- sum(distances[1:k] == q)
   fp <- k
   return (tp/fp)
 }
 
 calc.recall <- function(q, db, distances, k) {
-  tp <- sum(distances$target[1:k] == q)
+  tp <- sum(distances[1:k] == q)
   fn <- sum(db[,1] == q)
   return (tp/fn)
 }
 
 # calcular precision e recall
 calc.prec_recall <- function(db, qy, distances, k) {
-  
+  print("Calculando precision recall")
   l <- list()
   qrow <- nrow(qy)
   ks <- c(seq(5, 100, by = 5))
@@ -181,28 +129,33 @@ calc.prec_recall <- function(db, qy, distances, k) {
     precision <- c()
     recall <- c()
     for (q in c(1:qrow)) {
-       precision <- c(precision, calc.precision(qy[q,1], db, distances[[q]], ks[k]))
-       recall <- c(recall, calc.recall(qy[q,1], db, distances[[q]], ks[k]))
+      precision <- c(precision, calc.precision(distances[q,1], distances[q,-1], ks[k]))
+      recall <- c(recall, calc.recall(distances[q,1], db, distances[q,-1], ks[k]))
     }
     l[[k]] <- list(k=ks[k],  
-                     precision = precision, precision_mean=mean(precision), 
-                     recall = recall,  recall_mean=mean(recall))
+                   precision = precision, precision_mean=mean(precision), 
+                   recall = recall,  recall_mean=mean(recall))
   }
   return (l)
 }
 
-descriptor.get <- function(db, descriptor) {
+descriptor.get <- function(db, descriptor, rp_images = NULL) {
   
-  if (descriptor == "SAX"){
-    
+  if (descriptor == "HOG") {
+    hog_db <- sapply(rp_images, HOG,cells = 8, orientations = 6)
+    hog_db <- t(hog_db)
+    db_new <- cbind(as.data.frame(db[,1]),as.data.frame(hog_db))
   }
-  else if (descriptor == "RP") {
-    
+  else if (descriptor == "LBP") {
+    print("Extraindo LBP")
+    lbp_db <- sapply(rp_images, extrairLBP)
+    lbp_db <- t(lbp_db)
+    db_new <- cbind(as.data.frame(db[,1]),as.data.frame(lbp_db))
   }
   else{
-    db <- db
+    db_new <- db
   }
-  return(db)
+  return(db_new)
 }
 
 plot.classes <- function (db) {
@@ -214,10 +167,11 @@ plot.classes <- function (db) {
     df <- melt(data ,  id.vars = 'time', variable.name = 'series')
     #ggplot(df, aes(time,value)) + geom_line(aes(colour = series))
     ggplot(df, aes(time,value)) + geom_line() + facet_grid(series ~ .) + ggtitle(paste("Classe", i))
+    ggsave(paste("classe_",i, ".png",sep = ""))
   }
 }
 
-plot.prec_recall <- function(prec_recall) {
+plot.prec_recall <- function(prec_recall, title) {
   p <- c()
   r <- c()
   for (l in prec_recall) {
@@ -225,76 +179,71 @@ plot.prec_recall <- function(prec_recall) {
     r <- c(r, l$recall_mean)
   }
   data <- data.frame(precision=p, recall=r)
-  ggplot(data = data) + geom_point(aes(x=recall, y=precision)) + geom_line(aes(x=recall, y=precision))
+  ggplot(data = data) + geom_point(aes(x=recall, y=precision)) + geom_line(aes(x=recall, y=precision)) + ggtitle(title)
+  ggsave(paste(title, ".png", sep=""))  
+}
+
+check.normalizado <- function (db) {
+  # check mean = 0 , sd = 1 (dados normalizados?)
+  apply(db[,2:129], 1, mean)
+  apply(db[,2:129], 1, sd)
 }
 # ----------------------------------------------------------
+main.processar <- function(db_original, qy_original, tipo, dist.function, db_rp = NULL, qy_rp = NULL) {
+  
+ # db_desc <- descriptor.get(db_original, tipo, db_rp)
+ # qy_desc <- descriptor.get(qy_original, tipo, qy_rp)
+  
+  #for tests
+  #qy_desc <- qy_desc[1:10,]
+  
+  distances <- dist.calc(db_desc, qy_desc, dist.function)
+  distances <- cbind(qy_original[,1], distances)
+  
+  prec_recall <- calc.prec_recall(db_desc, qy_desc, distances)
+  
+  #plot.prec_recall(prec_recall)
+  
+  return (list("type" = tipo, "function"=dist.function, "distance" = distances, "prec_recall" = prec_recall))
+}
+
+main <- function () {
+  
+  summary(db_original[,-1])
+  
+  episilon <- sd(apply(db_original, 1, sd))
+  
+  plot.classes(db_original)
+  
+  #ggplot(data=db_original,aes(x=V1)) + geom_histogram(bins = 15, fill=I("blue"), col=I("black"),alpha = .4)
+  #ggsave("classes_histogram.png")
+  
+  check.normalizado(db_original)
+  check.normalizado(qy_original)
+
+  #normal_l2 <- main.processar(db_original, qy_original, "None", dist.L2)
+  #normal_cos <- main.processar(db_original, qy_original, "None", dist.cos)
+  
+  #dtw_l2 <- main.processar(db_original, qy_original, "None", dist.dtw_L2)
+  #dtw_cos <- main.processar(db_original, qy_original, "None", dist.dtw_L2)
+  
+  lbp_l1 <- main.processar(db_original, qy_original, "LBP", dist.L1, rp, queries_rp)
+  #lbp_l2 <- main.processar(db_original, qy_original, "LBP", dist.L2, rp, queries_rp)
+  #lbp_cos <- main.processar(db_original, qy_original, "LBP", dist.cos, rp, queries_rp)
+  
+  #hog_l2 <- main.processar(db_original, qy_original, "HOG", dist.L2, rp, queries_rp)
+  #hog_cos <- main.processar(db_original, qy_original, "HOG", dist.cos, rp, queries_rp)
+  
+  # plotar precision x recall todas as buscas
+  # ggplot()
+}
 
 # Main
 db_original <- read.csv("SwedishLeaf_TRAIN.csv", header = F)
 qy_original <- read.csv("SwedishLeaf_TEST.csv", header = F)
 
-summary(db_original[,2:129])
+# gera imagens do recurrence plot
+#rp <- recurrence_plot(db_original)
+#queries_rp <- recurrence_plot(qy_original)
 
-episilon <- sd(apply(db_original, 1, sd))
-
-#plot.classes(db_original)
-
-# check mean = 0 , sd = 1 (dados normalizados?)
-apply(db_original[,2:129], 1, mean)
-apply(db_original[,2:129], 1, sd)
-
-apply(qy_original[,2:129], 1, mean)
-apply(qy_original[,2:129], 1, sd)
-
-
-db_desc <- descriptor.get(db_original, "None")
-qy_desc <- descriptor.get(qy_original, "None")
-
-qy_desc <- qy_desc[1:20,]
-
-start_time <- Sys.time()
-distances <- dist.calc(db_desc, qy_desc, dist.L2)
-end_time <- Sys.time()
-print(end_time - start_time)
-
-prec_recall <- calc.prec_recall(db_desc, qy_desc, distances)
-
-plot.prec_recall(prec_recall)
-
-# Calcular recurrence Plot para cada item 
-rp <- list()
-for (i in c(1:length(db_original[,1]))) {
-  print(paste("Calculando Recurrence plot da Imagem: ",i))
-  rp[[i]] <- recurrence_plot(db_original[i, -1])
-}
-
-# Extrair LBP das imagens originais
-lbp_originais <- sapply(rp, extrairLBP)
-
-# Calcular Recurrence Plot e LBP das imagens de query, depois calcular as mais próximas
-queries_rp <- list()
-for (i in c(1:length(qy_original[,1]))) {
-  print(paste("Calculando Recurrence plot da Imagem: ",i))
-  queries_rp[[i]] <- recurrence_plot(qy_original[i, -1])
-}
-
-lbp_queries <- sapply(queries_rp, extrairLBP)
-
-# Calcula os 100 numeros mais próximos para as 625 queries
-MAIS_PROXIMOS <- 100
-
-proximosL1 <- matrix(nrow = length(lbp_queries[1,]), ncol = MAIS_PROXIMOS)
-for (i in c(1:length(lbp_queries[1,]))) {
-  proximosL1[i,] <- buscarMaisProximosL1(lbp_originais, lbp_queries[,i], MAIS_PROXIMOS)
-  proximosL1[i,] <- lista_imagens(proximos[i,])
-}
-
-proximosL2 <- matrix(nrow = length(lbp_queries[1,]), ncol = MAIS_PROXIMOS)
-for (i in c(1:length(lbp_queries[1,]))) {
-  proximosL2[i,] <- buscarMaisProximosL2(lbp_originais, lbp_queries[,i], MAIS_PROXIMOS)
-  proximosL2[i,] <- lista_imagens(proximos[i,])
-}
-
-
-# Calcular Precision e Recall para {5, 10, 15, 20, ..., 95, 100} imagens de retorno 
-
+main()
